ac79f6c29ba9077bc061bbc7bd9cc5800c93b065
Vitor <jotavitor2018@gmail.com>
2021-09-26 19:11:07 -0300

    Finalizando EJS

--- node-ejs/server.js
+++ node-ejs/server.js
@@ -3,11 +3,42 @@ const app = express();
 
 app.set("view engine", "ejs");
 
-app.get("/", function (req,res){
-    res.render("pages/index");
+app.get("/", function (req, res) {
+    const items = [
+        {
+            title: "D",
+            message: "esenvolvendo codigo"
+        },
+        {
+            title: "E",
+            message: "studando EJS"
+        },
+        {
+            title: "M",
+            message: "uito facil"
+        },
+        {
+            title: "A",
+            message: "plicacoes futuras"
+        },
+        {
+            title: "I",
+            message: "mpressionado com EJS"
+        },
+        {
+            title: "S",
+            message: "intaxe simples"
+        }
+    ];
+
+    const subtitle = "Estudando Webdevelopment"
+    res.render("pages/index", {
+        qualitys: items,
+        subtitle: subtitle,
+    });
 })
 
-app.get("/about", function (req,res){
+app.get("/about", function (req, res) {
     res.render("pages/about");
 })
 
